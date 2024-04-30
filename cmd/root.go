852d5b46e04de5406386da871b6885e77090d0fb
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-04-30 08:26:04 -0300

    Remove --help flag

--- cmd/root.go
+++ cmd/root.go
@@ -10,19 +10,10 @@ import (
 var (
 	wordCount = flag.Int("count", 8, "Number of words in the passphrase")
 	separator = flag.String("sep", "-", "Separator between words")
-    showUsage = flag.Bool("help", false, "Show usage information")
 )
 
 func Execute() {
 	flag.Parse()
-
-    if *showUsage {
-        fmt.Println("Generate secure passphrases using the diceware method\n")
-        fmt.Println("Usage: \n  passgen [options]\n")
-        fmt.Println("Options:")
-        flag.PrintDefaults()
-        return
-    }
 
 	words, err := gen.ReadWords()
 	if err != nil {
