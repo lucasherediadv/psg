c03e72e3852c737599fc532f0b1b14cce9ceb518
Lucas <151478033+lucasherediadv@users.noreply.github.com>
2024-05-10 17:15:08 -0300

    Generating markdown docs for passgen

--- cmd/root.go
+++ cmd/root.go
@@ -1,23 +1,33 @@
 package cmd
 
 import (
-	"os"
+	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "passgen",
-	Short: "Passphrase/password generator",
+	Short: "Passphrase and password generator",
 	Long:  `Generate secure passphrases and passwords`,
 }
 
-func Execute() {
+func Execute() error {
 	// Disable default completion command
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
 
-	err := rootCmd.Execute()
+	// Generate markdown documentation
+	err := doc.GenMarkdownTree(rootCmd, "./doc")
 	if err != nil {
-		os.Exit(1)
+		return fmt.Errorf("Error generating markdown documentation: %w", err)
 	}
+
+	// Execute the root command
+	err = rootCmd.Execute()
+	if err != nil {
+		return fmt.Errorf("Error executing root command: %w", err)
+	}
+
+	return nil
 }
